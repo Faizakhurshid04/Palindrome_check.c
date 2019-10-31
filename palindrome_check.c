654cb8898f64e55fd5cd730dffa4bb7334550f3a
Faizakhurshid04 <57167359+Faizakhurshid04@users.noreply.github.com>
2019-10-31 18:30:33 +0530

    Update palindrome_check.c

--- palindrome_check.c
+++ palindrome_check.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int n, revInteger = 0,remainder, origInteger;
+    int n, revInteger = 0,origInteger;
     printf("Enter an integer: ");
     scanf("%d", &n);
     origInteger = n;
   
     while( n!=0 )
     {
-        remainder = n%10;
+        int remainder = n%10;
         reversedInteger = reversedInteger*10 + remainder;
         n /= 10;
     }
